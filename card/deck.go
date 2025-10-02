8b88c9ab726e7976a60e0875c21f058c4686aaa1
sqi925 <sqi@Ss-MacBook-Pro.local>
2025-10-02 12:19:17 -0400

    on mac side test

--- card/deck.go
+++ card/deck.go
@@ -1,31 +1,32 @@
 package main
-//create a new type of  'deck'  
+
+//create a new type of  'deck'
 //which is a slice of strings
 import "fmt"
+
 type deck []string
 
-
 func newDeck() deck {
-	cards:=deck{}
-	cardSuit :=[]string{"spades","diamonds","heart","club"}
-	cardValue:=[]string{"ace","two","three","four "} 
-	
-	for _,suit :=range cardSuit{
-		for _,value :=range cardValue{
-			cards=append(cards,value+" of "+suit)
+	cards := deck{}
+	cardSuit := []string{"spades", "diamonds", "heart", "club"}
+	cardValue := []string{"ace", "two", "three", "four "}
+
+	for _, suit := range cardSuit {
+		for _, value := range cardValue {
+			cards = append(cards, value+"-  of -"+suit)
 		}
 	}
 
 	return cards
-	
+
 }
 
-func(d deck)print(){
-	for i,v:=range d{
-		fmt.Println(i,v)
+func (d deck) print() {
+	for i, v := range d {
+		fmt.Println(i, v)
 	}
 }
 
-func deal(d deck,handSize int)(deck,deck){   //type of deck, handsize of int, return two type both are deck
-	return d[:handSize],d[handSize:]
+func deal(d deck, handSize int) (deck, deck) { //type of deck, handsize of int, return two type both are deck
+	return d[:handSize], d[handSize:]
 }
