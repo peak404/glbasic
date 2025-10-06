3a7e69d7358ecfd01a7f16817ca9f0d6fa71eef3
sqi925 <sqi@Ss-MacBook-Pro.local>
2025-10-06 11:06:39 -0400

    struct

--- go/struct/main.go
+++ go/struct/main.go
@@ -1,2 +1,34 @@
 package main
+import(
+	"fmt"
+)
 
+type contactInfo struct{
+	email string
+	zipCode int
+
+}
+type person struct{
+	firstName string
+	lastName string
+	contactInfo
+}
+
+func main(){
+	// a :=person{"shen","qi"}
+
+	// var p person
+	// p.firstName = "dandan"
+	// p.lastName = "lin"
+
+	p:=person{
+		firstName:"dandan",
+		lastName:"lin",
+		contactInfo: contactInfo{
+			email:"www.ddl.com",zipCode:11111,
+		},
+	}
+	fmt.Printf("%+v",p)
+	
+	
+} 
