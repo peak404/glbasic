78b39be6a2f724b4766de96142ff12e018cf3e5f
sqi925 <sqi@Ss-MacBook-Pro.local>
2025-10-06 16:33:55 -0400

    interfaces

--- go/map/main.go
+++ go/map/main.go
@@ -1,1 +1,31 @@
-package map
+package main
+
+import "fmt"
+
+func main(){
+	colors := map[string]string{
+		"red":"#ff0000",
+		"green":"#f3434",
+		"yellow":"#f34ff",
+	}
+	 
+	// var colors map[string]string
+
+	// colors :=make(map[int]string)
+
+	// colors[3]="#fff3fds"
+	// delete(colors,3)
+
+
+
+
+	
+	// fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(color map[string]string){
+	for i,v:=range color{
+		fmt.Printf("%s  <-> %s\n",i,v) 
+	}
+}
