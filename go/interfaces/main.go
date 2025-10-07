d08fc7932cca77a3336c869dcf987e7608d8e110
sqi925 <sqi@Ss-MacBook-Pro.local>
2025-10-07 16:19:48 -0400

    update https

--- go/interfaces/main.go
+++ go/interfaces/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
+import(
+	"fmt"
 	
+	"net/http"
+)
+
+func main(){
+	resp,err :=http.Get("http://www.google.com")
+	if err !=nil{
+		fmt.Println("error: ",err)
+		return
+	}
+	resp.Body.Close()
+	fmt.Println(resp.StatusCode)
+
 }
