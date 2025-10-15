7511fb69ca9a6d53955c0ffff8e8e783bdd69409
shen <sqi1991@yahoo.com>
2025-10-15 01:22:46 -0400

    channels

--- go/channels/main.go
+++ go/channels/main.go
@@ -1,4 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+
+)
+
+
+
  func main(){
-	
+	links:=[]string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+
+	}
+	 
+	c:=make(chan string)
+
+
+	for _,link:=range links{
+		 go checkLink(link,c)
+	}
+
+	fmt.Println(<-c)
+
+
+ }
+
+ func checkLink(link string,c chan string){
+	_, err := http.Get(link)
+	if err!=nil{
+		fmt.Println("something wrong",err)
+		c<- "something wrong fro m channel"
+		return
+	}
+	fmt.Println(link+" is up")
+	c<- "it's up from channel"
+	// fmt.Println(resp.Status)
  }
