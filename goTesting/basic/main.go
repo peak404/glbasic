c5dc3a78e46b6d935bbfb278bd85215b702b392a
shen <sqi1991@yahoo.com>
2025-10-17 22:14:01 -0400

    new  go

--- goTesting/basic/main.go
+++ goTesting/basic/main.go
@@ -3,5 +3,17 @@ import "fmt"
 
 
 func main(){
-	fmt.Println("hdi")
+	//print emoji
+	fmt.Println("😂")
+	a1,b1,c1,d1,e1 :=0,1,2,3,4
+
+	//binary hex
+	fmt.Printf("%v \t %b \t %X\n",a1,a1,a1)
+	fmt.Printf("%v \t %b \t %X\n",b1,b1,b1)
+	fmt.Printf("%v \t %b \t %X\n",c1,c1,c1)
+	fmt.Printf("%v \t %b \t %X\n",d1,d1,d1)
+	fmt.Printf("%v \t %b \t %X\n",e1,e1,e1)
+
+	
+
 }
