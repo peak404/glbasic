efa32108e421b9dac0c2602da4a1956572056809
shen <sqi1991@yahoo.com>
2025-10-21 08:03:21 -0400

    pipeline

--- goTesting/webApp/8-function/data_format/main.go
+++ goTesting/webApp/8-function/data_format/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"string"
 	"strings"
 	"text/template"
 )
@@ -12,12 +11,12 @@ var tpl *template.Template
 
 type sage struct{
 	Name string
-	Word strings
+	Word string
 }
 
 type car struct{
 	Manufacturer string
-	Model strings
+	Model string
 	Door int
 }
 	
@@ -29,7 +28,7 @@ var fm = template.FuncMap{
 }
 
 func init(){
-	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("tpl.gohtml"))
+	tpl = template.Must(template.New("").Funcs(fm).ParseFiles("8-function/tpl.gohtml"))
 }
 
 func firstThree(s string) string{
@@ -42,38 +41,37 @@ func firstThree(s string) string{
 
 
 
-func main()
-{
+func main(){
 	b:=sage{
-		Name: "budda"
-		Word: "peace forever"
+		Name: "budda",
+		Word: "peace forever",
 	}
 
-	s:sage{
-		Name : "shen"
-		word : "happy happy happy"
+	s:=sage{
+		Name : "shen",
+		Word : "happy happy happy",
 	}
 
-	f:car{
-		Manufacturer:"ford"
-		Model:"fx350"
-		Door:"4"
+	f:=car{
+		Manufacturer:"ford",
+		Model:"fx350",
+		Door: 4,
 	}
 
-	t:car{
-		Manufacturer:"toyota"
-		Model:"avalon666"
+	t:=car{
+		Manufacturer:"toyota",
+		Model:"avalon666",
 	}
 
 	sages:=[]sage{b,s}
 	cars:=[]car{f,t}
 
 	datas:=struct{
-		wisdom []sage
-		transport []car
+		Wisdom []sage
+		Transport []car
 	}{
-		sages
-		cars
+		sages,
+		cars,
 	}
 
 	err :=tpl.ExecuteTemplate(os.Stdout,"tpl.gohtml",datas)
