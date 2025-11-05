ad8a457f6827b5e541544bd8a9ccad5a744aa809
shen <sqi1991@yahoo.com>
2025-11-04 20:19:41 -0500

    leetocode

---
+++ goTesting/leetcode/arrayAndString/twoPointer/main.go
@@ -0,0 +1,20 @@
+package main
+
+
+import(
+	"fmt"
+)
+
+func main(){
+
+	isSubsequence()
+
+}
+
+
+func isSubsequence(s string, t string) bool{
+	left,right :=0,0
+	for left<len(s)  && right <len(t)
+
+}
+
