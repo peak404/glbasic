37a663a8d6419eb5e9ff4c2640ab56c51f86d1d1
shen <sqi1991@yahoo.com>
2025-11-05 21:53:12 -0500

    392 subsequence

--- goTesting/leetcode/arrayAndString/twoPointer/main.go
+++ goTesting/leetcode/arrayAndString/twoPointer/main.go
@@ -7,14 +7,29 @@ import(
 
 func main(){
 
-	isSubsequence()
+	fmt.Println(isSubsequence("abc","asgrc"))
+
 
 }
 
 
 func isSubsequence(s string, t string) bool{
 	left,right :=0,0
-	for left<len(s)  && right <len(t)
+	for left<len(s) && right<len(t){
+		if s[left] == t[right]{
+			left++
+			right++
+		}else{
+			right++
+		}
+	}
+	if left == len(s){
+			return true
+		}
+	return false
+
+	
+
 
 }
 
